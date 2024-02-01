2be16e3d8f17e354ef49e44e43ff6969567cbeda
EnderIce2 <enderice2@users.noreply.github.com>
2024-02-01 04:41:38 +0200

    Update Fennix configuration in gcc

--- gcc/config/fennix.h
+++ gcc/config/fennix.h
@@ -1,37 +1,59 @@
 
-#undef TARGET_FENNIX
+#undef  TARGET_FENNIX
 #define TARGET_FENNIX 1
 
-#undef LIB_SPEC
+#undef  LIB_SPEC
 #define LIB_SPEC "-lc"
 
-#undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+#undef  CPP_SPEC
+#define CPP_SPEC "%{posix:-D_POSIX_SOURCE} %{pthread:-D_POSIX_THREADS}"
 
-#undef ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC        \
+ "crti.o%s                    \
+  %{!shared:                  \
+    %{!static:                \
+      crtbeginS.o%s Scrt1.o%s \
+    }                         \
+  }                           \
+  %{static:crtbegin.o%s crt1.o%s}"
 
-#undef STANDARD_STARTFILE_PREFIX
+#undef  ENDFILE_SPEC
+#define ENDFILE_SPEC        \
+ "crtn.o%s                  \
+  %{!shared:                \
+    %{!static:              \
+      crtendS.o%s           \
+    }                       \
+  }                         \
+  %{static:crtend.o%s}"
+
+#undef  STANDARD_STARTFILE_PREFIX
 #define STANDARD_STARTFILE_PREFIX "/lib/"
 
-#undef LINK_SPEC
-#define LINK_SPEC                      \
-"%{shared:-shared}                     \
- %{static:-static}                     \
- %{!shared:                            \
-   %{!static:                          \
-     %{rdynamic:-export-dynamic}       \
-     -dynamic-linker /lib/ld.so        \
-    }                                  \
-  } -z max-page-size=4096"
+#undef  LINK_SPEC
+#define LINK_SPEC                       \
+ "%{shared:-shared}                     \
+  %{static:-static}                     \
+  %{!shared:                            \
+    %{!static:                          \
+      %{rdynamic:-export-dynamic}       \
+    }                                   \
+  }                                     \
+  %{!static:                            \
+    %{!dynamic-linker:                  \
+      -dynamic-linker=/lib/ld.so        \
+    }                                   \
+  }                                     \
+  -z max-page-size=4096"
 
-#undef TARGET_OS_CPP_BUILTINS
-#define TARGET_OS_CPP_BUILTINS()         \
-    do                                   \
-    {                                    \
-        builtin_define("__fennix__");    \
-        builtin_define("__unix__");      \
-        builtin_assert("system=fennix"); \
-        builtin_assert("system=unix");   \
-        builtin_assert("system=posix");  \
-    } while (0);
+#undef  TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()       \
+  do                                   \
+  {                                    \
+      builtin_define("__fennix__");    \
+      builtin_define("__unix__");      \
+      builtin_assert("system=fennix"); \
+      builtin_assert("system=unix");   \
+      builtin_assert("system=posix");  \
+  } while (0);
